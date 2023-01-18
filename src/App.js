721fb251f1d0be7a18d13a12ff53c62bf691b67e
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-01-18 10:46:28 +0100

    added new component Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./Form";
+
+const tasks = [
+  {id:1, content: "kup mleko", done: true},
+  {id:2, content: "kup bułki", done: false},
+];
+
+const hideDoneTasks = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className="header"> Lista zadań </h1>
+
+      <section className="container">
+        <h2 className="container__header">Dodaj nowe zadanie</h2>
+        <Form />
+      </section>
+
+      <section className="container">
+        <header className="container__header">
+          <h2 className="container__header--removeMargin">Lista zadań</h2>
+          <div className="container__buttons"></div>
+        </header>
+        <ul className="tasks"></ul>
+      </section>
+    </main>
   );
 }
 
