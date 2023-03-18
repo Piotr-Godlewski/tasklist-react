3514449977e2218652a594ed1bafc99f0406697e
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-03-18 18:16:44 +0100

    created file with theme

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    color: {
+        teal: "#008080",
+        silver: "#ccc",
+        white: "#fff",
+        black: "#000",
+        alto: "#ddd",
+        gallery: "#eee",
+        forestGreen: "#26b526",
+        crimson: "#DC143C"
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
