bed778d86cf44a1946f6657e5d29360d31f17c19
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-04-11 14:11:19 +0200

    added tasks, hideDone form selectTasks

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,19 +1,26 @@
+import { useSelector, useDispatch } from "react-redux"
 import { ContainerButtons, Button } from "./styled"
+import { selectTasks, toogleHideDone } from "../tasksSlice"
 
-const Buttons = ({ tasks, hideDoneTasks, toggleHideDoneTasks, setAllDone }) => (
-    tasks.length > 0 && (
-        <ContainerButtons>
-            <Button onClick={toggleHideDoneTasks}>
-                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-            </Button>
-            <Button
-                onClick={setAllDone}
-                disabled={tasks.every(({ done }) => done)}
-            >
-                Ukończ wszystkie
-            </Button>
-        </ContainerButtons>
+const Buttons = ({ setAllDone }) => {
+    const {tasks, hideDone} = useSelector(selectTasks);
+    const dispatch = useDispatch();
+
+    return (
+        tasks.length > 0 && (
+            <ContainerButtons>
+                <Button onClick={ () => dispatch(toogleHideDone())}>
+                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                </Button>
+                <Button
+                    onClick={setAllDone}
+                    disabled={tasks.every(({ done }) => done)}
+                >
+                    Ukończ wszystkie
+                </Button>
+            </ContainerButtons>
+        )
     )
-)
+}
 
 export default Buttons
