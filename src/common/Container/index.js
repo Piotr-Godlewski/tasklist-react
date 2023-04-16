bb749a7083eb27bf86a0f5ac5b49dc20284e64a6
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-04-16 14:04:13 +0200

    deleted unused files

--- src/common/Container/index.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Container = styled.main`
-    display: grid;
-    justify-content: center;
-    grid-template-columns: minmax(0, 1000px);
-`;
-
-export default Container
--- src/common/Header/index.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Header = styled.h1`
-margin: 0 15px;
-padding: 20px 0;
-`
-
-export default Header
