5fc05df9dc5a115a3133d1006af54fc785da21df
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-05-06 20:34:53 +0200

    persist tasks in localStorage (again)

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,6 +1,7 @@
-import { call, delay, put, takeEvery } from "redux-saga/effects"
-import { fetchExampleTasks, setTasks } from "./tasksSlice"
+import { call, delay, put, select, takeEvery, takeLatest } from "redux-saga/effects"
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice"
 import { getExampleTasks } from "./getExampleTasks"
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
 
 function* fetchExampleTasksHandler() {
     try {
@@ -12,6 +13,12 @@ function* fetchExampleTasksHandler() {
     }
 }
 
-export function* watchFetchExampleTasks() {
-    yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler)
 }
