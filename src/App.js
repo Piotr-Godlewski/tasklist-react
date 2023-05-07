20167d8c213bbb3d302fd582d29000eeb414471d
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-05-07 12:55:59 +0200

    Added Navigation Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, NavLink, Switch, Route, Redirect } from "react-router-dom";
+import AuthorPage from "./features/author/AuthorPage";
+import TaskPage from "./features/tasks/TaskPage";
+import TasksPage from "./features/tasks/TasksPage";
 
-const App = () => <Tasks />;
+const App = () => {
+    return (
+        <HashRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/tasks">Zadania</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/author">O autorze</NavLink>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route path="/tasks/:id">
+                        <TaskPage />
+                    </Route>
+                    <Route path="/tasks">
+                        <TasksPage />
+                    </Route>
+                    <Route path="/author">
+                        <AuthorPage />
+                    </Route>
+                    <Route path="/">
+                        <Redirect to="tasks" />
+                    </Route>
+                </Switch>
+            </nav>
+        </HashRouter>
+    )
+}
 
 export default App
