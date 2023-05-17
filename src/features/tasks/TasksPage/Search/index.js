758ee17f469193a14c1ef8e0cff2619436db3748
Piotr Godlewski <godlewskipiotr.pg@gmail.com>
2023-05-17 18:57:26 +0200

    replace useLocation and History with a custom hook

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,34 +1,28 @@
-import { useLocation, useHistory } from "react-router-dom/cjs/react-router-dom";
 import Input from "../../Input";
 import { Wrapper } from "./styled";
 import searchQueryParamName from "../searchQueryParamName"
+import { useQueryParameter, useReplaceQueryParameter } from "../queryParameters";
 
 const Search = () => {
-    const location = useLocation();
-    const history = useHistory();
-    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
 
-    const onInputChange = ({target}) => {
-        const searchParams = new URLSearchParams(location.search);
+    const onInputChange = ({ target }) => {
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : undefined,
+        });
+    };
 
-        if (target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        } else {
-            searchParams.set(searchQueryParamName, target.value);
-        }
-
-        history.push(`${location.pathname}?${searchParams.toString()}`)
-    }
-    
-    return (
-        <Wrapper>
-            <Input 
+return (
+    <Wrapper>
+        <Input
             placeholder="Filtruj zadania"
             value={query || ""}
             onChange={onInputChange}
-            />
-        </Wrapper>
-        )
+        />
+    </Wrapper>
+)
 };
 
 export default Search
